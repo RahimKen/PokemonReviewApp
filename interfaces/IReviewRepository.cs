1b050f12843cd26f0497dc99ca052fec96211ca7
rahim <kendjouhabderrahim3@gmail.com>
2023-03-01 06:40:56 +0100

    Get Requests Done

---
+++ interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int reviewId);
+        
+    }
+}
